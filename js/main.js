e252e62dbb2cc197bb7c8463ea799ba1ff1ff61a
Geoff Agnew <gagnew@gmail.com>
2016-05-31 21:53:42 -0400

    css for ui.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
-var racism = ['When Mexico sends its people, they’re not sending the best. They’re not sending you, they’re sending people that have lots of problems and they’re bringing those problems with us. They’re bringing drugs. They’re bring crime. They’re rapists… And some, I assume, are good people.', 'The concept of global warming was created by and for the Chinese in order to make U.S. manufacturing non-competitive.', 'Donald J. Trump is calling for a total and complete shutdown of Muslims entering the United States until our country\'s representatives can figure out what is going on.', 'When these people walk in the room, they don’t say, ‘Oh, hello! How’s the weather? It’s so beautiful outside. Isn’t it lovely? How are the Yankees doing? Oh they’re doing wonderful. Great.’ [Asians] say, ‘We want deal!', '#JebBush has to like the Mexican illegals because of his wife.', 'So true. Jeb Bush is crazy, who cares that he speaks Mexican, this is America, English!!', 'Laziness is a trait in blacks.', 'I have a great relationship with the blacks. I’ve always had a great relationship with the blacks.', 'Our great African American President hasn\'t exactly had a positive impact on the thugs who are so happily and openly destroying Baltimore!'];
+var racism = ['When Mexico sends its people, they’re not sending the best. They’re not sending you, they’re sending people that have lots of problems and they’re bringing those problems with us. They’re bringing drugs. They’re bring crime. They’re rapists… And some, I assume, are good people.', 'The concept of global warming was created by and for the Chinese in order to make U.S. manufacturing non-competitive.', 'Donald J. Trump is calling for a total and complete shutdown of Muslims entering the United States until our country\'s representatives can figure out what is going on.', 'When these people walk in the room, they don’t say, ‘Oh, hello! How’s the weather? It’s so beautiful outside. Isn’t it lovely? How are the Yankees doing? Oh they’re doing wonderful. Great.’ [Asians] say, ‘We want deal!', '#JebBush has to like the Mexican illegals because of his wife.', 'So true. Jeb Bush is crazy, who cares that he speaks Mexican, this is America, English!!', 'Laziness is a trait in blacks.', 'I have a great relationship with the blacks. I’ve always had a great relationship with the blacks.', 'Our great African American President hasn\'t exactly had a positive impact on the thugs who are so happily and openly destroying Baltimore!', '“His name is Gonzalo Curiel. And he is not doing the right thing ... [He] happens to be, we believe, Mexican.” (Curiel, who is Hispanic, is American and was born in Indiana.)'];
 
-var misogyny = ['You know, it really doesn’t matter what the media write as long as you’ve got a young, and beautiful, piece of ass.', 'If I were running ‘The View’, I’d fire Rosie O’Donnell. I mean, I’d look at her right in that fat, ugly face of hers, I’d say ‘Rosie, you’re fired.’', 'You could see there was blood coming out of her eyes, blood coming out of her, wherever.', 'But she was gonna beat – she was favored to win and she got schlonged. She lost. I mean, she lost.', 'Heidi Klum. Sadly, she\'s no longer a 10.'];
+var misogyny = ['You know, it really doesn’t matter what the media write as long as you’ve got a young, and beautiful, piece of ass.', 'If I were running ‘The View’, I’d fire Rosie O’Donnell. I mean, I’d look at her right in that fat, ugly face of hers, I’d say ‘Rosie, you’re fired.’', 'You could see there was blood coming out of her eyes, blood coming out of her, wherever.', 'But she was gonna beat – she was favored to win and she got schlonged. She lost. I mean, she lost.', 'Heidi Klum. Sadly, she\'s no longer a 10.', '[Kelly] gets out and she starts asking me all sorts of ridiculous questions. You could see there was blood coming out of her eyes. Blood coming out of her wherever.'];
 
 var weird = ['I’ve said if Ivanka weren’t my daughter, perhaps I’d be dating her.', 'My fingers are long and beautiful, as, it has been well documented, are various other parts of my body.', 'All the women on The Apprentice flirted with me — consciously or unconsciously. That’s to be expected.', 'I watched when the World Trade Center came tumbling down. And I watched in Jersey City, New Jersey, where thousands and thousands of people were cheering as that building was coming down. Thousands of people were cheering.', 'It is very hard for them to attack me on looks, because I am so good looking.', 'Well, somebody’s doing the raping, Don! I mean somebody’s doing it! Who’s doing the raping? Who’s doing the raping?' , 'While @BetteMidler is an extremely unattractive woman, I refuse to say that because I always insist on being politically correct.', 'I know words, I have the best words. I have the best, but there is no better word than stupid.'];
 
@@ -68,7 +68,7 @@ function clearData(e) {
 	document.getElementById('donald-inputs').reset();
 }
 
-// var btn = document.getElementById('btn');
+var btnSubmit = document.getElementById('btn-submit');
 // btn.addEventListener('click', function(e) {
 // 	getInputs(e);
 // }, false);
@@ -82,6 +82,8 @@ btnReset.addEventListener('click', function(e) {
 	btnReset.setAttribute('class', 'hide');
 }, false);
 
+var resultsContainer = document.getElementById('results');
+resultsContainer.setAttribute('class', 'hide');
 
 // Event handler for form validation nested in IIFE
 (function() {
@@ -125,11 +127,19 @@ btnReset.addEventListener('click', function(e) {
 			// alert('Congrats you passed');
 			getInputs(e);
 			btnReset.removeAttribute('class', 'hide');
+			btnSubmit.setAttribute('class', 'hide');
+			resultsContainer.removeAttribute('class', 'hide');
 		} else {
-			alert('One or more inputs have no value. Please correct.');
+			alert('Please fill in the missing fields.');
 		}
 
 	}); // ----- end of event listener
+
+	var resetClick = btnReset.addEventListener('click', function(e) {
+		e.preventDefault();
+		btnSubmit.removeAttribute('class', 'hide');
+		resultsContainer.setAttribute('class', 'hide');
+	});
 
 	// function to display error message
 	function errorDisplayFailed(el) {
